0b10a370c027887cbd4c3a35cdb48f77767a5a70
Ray <ngorui15@gmail.com>
2023-04-05 17:47:49 +0800

    First Commit
    First Commit 4/5/2023

---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const express = require('express');
+// const routeMovies=require('./routes/routeMovies');
+// const routeComments = require('./routes/routeComments');
+const bodyParser = require('body-parser');
+
+
+var app = express();
+var host = "127.0.0.1";
+var port = 3232;
+var home_file = "/index.html";
+
+app.use(express.static('./project'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routeMovies.routeMovies(app);
+// routeComments.routeComments(app);
+
+function gotoIndex(f, request, respond) {
+    respond.sendFile(__dirname + f);
+}
+
+app.get(home_file, gotoIndex);
+
+// Starts the Web Server
+var server = app.listen(port, host, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
+});
