4a7f47f4d7ce3ec773b09a6f95029fee1948164d
Ray <ngorui15@gmail.com>
2023-04-07 22:05:10 +0800

    Forth Commit
    4/7/2023

--- project/js/loading.js
+++ project/js/loading.js
@@ -10,7 +10,7 @@ function move() {
       i = 1;
       var elem = document.getElementById("myBar");
       var width = 20;
-      var id = setInterval(frame, 20);
+      var id = setInterval(frame, 10);
       function frame() {
         if (width >= 100) {
           clearInterval(id);
--- project/js/navbar.js
+++ project/js/navbar.js
@@ -1,19 +1,75 @@
-const a = document.getElementById('nav1');
-const b = document.getElementById('nav2');
-const c = document.getElementById('nav3');
+function makeActive() {
+    var x = document.getElementById("movie");
+    var c = document.getElementById("moviepage");
 
-function nav1() {
-    a.classList.add('active');
-    b.classList.remove('active');
-    c.classList.remove('active');
-}
-function nav2() {
-    a.classList.remove('active');
-    b.classList.add('active');
-    c.classList.remove('active');
-}
-function nav3() {
-    a.classList.remove('active');
-    b.classList.remove('active');
-    c.classList.add('active');
-}
+    var y = document.getElementById("menu");
+    var b = document.getElementById("menupage");
+
+    var z = document.getElementById("feedback");
+    var a = document.getElementById("feedbackpage");
+
+    x.classList.add("active");
+    y.classList.remove("active");
+    z.classList.remove("active");
+
+    if (c.style.display == "block") {
+        a.style.display="none";
+        b.style.display="none";
+    } else {
+        $("#moviepage").fadeIn()
+        c.style.display="block";
+        b.style.display="none";
+        a.style.display="none";
+    }
+ }
+
+ function makeActive1() {
+    var x = document.getElementById("movie");
+    var c = document.getElementById("moviepage");
+
+    var y = document.getElementById("menu");
+    var b = document.getElementById("menupage");
+
+    var z = document.getElementById("feedback");
+    var a = document.getElementById("feedbackpage");
+
+    y.classList.add("active");
+    z.classList.remove("active");
+    x.classList.remove("active");
+
+
+    if (b.style.display == "block") {
+        a.style.display="none";
+        c.style.display="none";
+    } else {
+        $("#menupage").fadeIn()
+        b.style.display="block";
+        a.style.display="none";
+        c.style.display="none";
+    }
+ }
+
+ function makeActive2() {
+    var x = document.getElementById("movie");
+    var c = document.getElementById("moviepage");
+
+    var y = document.getElementById("menu");
+    var b = document.getElementById("menupage");
+
+    var z = document.getElementById("feedback");
+    var a = document.getElementById("feedbackpage");
+
+    z.classList.add("active");
+    y.classList.remove("active");
+    x.classList.remove("active");
+
+    if (a.style.display == "block") {
+        b.style.display="none";
+        c.style.display="none";
+    } else {
+        $("#feedbackpage").fadeIn()
+        a.style.display="block";
+        b.style.display="none";
+        c.style.display="none";
+    }
+ }
